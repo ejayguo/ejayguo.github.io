b0e3b07c5554ed42e3a07a0efdbb2eb12ed96976
EJay.Guo <ejay.guo@gmail.com>
2021-12-26 21:41:07 -0500

    basic frame done

--- React/ejay-app/src/App.js
+++ React/ejay-app/src/App.js
@@ -1,24 +1,36 @@
 import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './components/pages/Home';
+import Resume from './components/pages/Resume';
+import Education from './components/pages/Education';
+import Project from './components/pages/Project';
+
+
+import Navbar from './components/Navbar';
+
+
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          
+          <Route path='/education' element={<Education/>} />
+          <Route path='/project' element={<Project/>} />
+
+          <Route path='/resume' element={<Resume/>} />
+          
+        </Routes>
+      </Router>
+    </>
+
+    // </div>
   );
 }
 
---
+++ React/ejay-app/src/components/pages/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import HeroSection from '../HeroSection';
+import Cards from '../Cards';
+
+
+import '../../App.css';
+
+function Home() {
+    return (
+        <>
+            <HeroSection/>
+            <Cards/>
+        </>
+        // <div>
+            // {/* <h1>HOME TEXT</h1> */}
+            
+            // {/* <h1>HOME TEXT</h1> */}
+        // </div>
+    )
+}
+
+export default Home;
